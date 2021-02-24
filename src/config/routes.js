fbe03e0c1f645feb88826d0c943b9b027d55ed18
victordoyle <victor-doyle@outlook.com>
2021-02-23 22:35:58 -0500

    added base routes and homepage+landing skeleton

---
+++ src/config/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+/* Pages */
+import LandingPage from '../pages/LandingPage'
+import Homepage from '../pages/Homepage'
+
+export default (
+    <Switch>
+        <Route exact path='/' component={ LandingPage } />
+        <Route path='/home' component={ Homepage } />
+        {/* 404 ROUTE */}
+        {/* <Route component={NotFound} /> */}
+    </Switch>
+)
+
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import '../styles/LandingPage.css'
+
+
+
+const LandingPage = () => {
+  return (
+  <>
+<h1> hello world</h1>
+
+  </>
+  );
+}
+
+export default LandingPage;
