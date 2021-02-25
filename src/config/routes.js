1fb2c39cd78b0ce6e8dbdbb44b93defc5565a835
victordoyle <victor-doyle@outlook.com>
2021-02-25 12:29:25 -0500

    added CNAME

--- src/config/routes.js
+++ src/config/routes.js
@@ -10,10 +10,10 @@ import Space22 from '../pages/Works/Space22'
 export default (
     <Switch>
         <Route exact path='/portfolio2' component={ LandingPage } />
-        <Route path='/about' component={ About } />
-        <Route path='/works' component={ Works } />
+        <Route path='/portfolio2/about' component={ About } />
+        <Route path='/portfolio2/works' component={ Works } />
         {/* Works Detailed Pages */}
-        <Route path='/space22' component={ Space22 } />
+        <Route path='/portfolio2/space22' component={ Space22 } />
         {/* 404 ROUTE */}
         {/* <Route component={NotFound} /> */}
     </Switch>
