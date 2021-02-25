f142ae611b90176574f00729b802c030ef60faac
victordoyle <victor-doyle@outlook.com>
2021-02-24 22:45:25 -0500

    finished project one content

--- src/pages/Works/Space22.js
+++ src/pages/Works/Space22.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Space22.css'
-import { Container, Header, Image } from 'semantic-ui-react'
-/* image one  */
-/* import space22hero from "../../images/space22hero.jpeg"
- */
+import { Container, Header, Image, Progress, Icon } from 'semantic-ui-react'
+/* hero one  */
+import space22hero from "../../images/space22hero.jpg"
+import space22heroTwo from "../../images/space22heroTwo.jpg"
+
 
 function Space22 () {
     const [innerText, setInnerText] = useState("victor.")
@@ -22,7 +23,7 @@ function Space22 () {
         <p className="projectDescription"> Audio Visual Interactive Website</p>
         </div>
 
-        <Image className="heroImage" src={""}/>
+        <Image className="heroImage" src={space22hero}/>
         </div>
 
         {/* text content */}
@@ -39,9 +40,54 @@ function Space22 () {
         Achieved more than 10,000 streams in under 3 months of beginning to stream on platforms such as Spotify and Apple Music. After analyzing consumer feedback, Spotify listener analytics and overall streaming data, I decided to take music production and apply innovative ways to experience each song. I built an interactive website to showcase select songs and deliver a visual journey to listeners. The website is built in HTML, CSS and Javascript while utilizing the power of Jquery for animations.
 
 </p>
+
+            <Progress progress='percent' value={41.7}  total={100} color='red' active >
+            <Icon name="html5"/> HTML 5
+            </Progress>
+            <Progress progress='percent' value={37.5} total={100} color='red' active >
+            <Icon name="css3" /> CSS3
+            </Progress>
+            <Progress progress='percent' value={20.8} total={100} color='red' active >
+            <Icon name="js"/> Javascript
+            </Progress>
+
+
+
+            <Header className="contentTitle" as='h2'>The Goal</Header>
+        
+        <p className="contentMiniDescription"> 
+        The immediate goal of this website is to bring to life a select four songs from the Space-22 Albums. These are: "Purple", "The First Space Walk", "Ad Orionis" and "Gravity Rider". Each of these songs are given their own "Chapter" experience. These can be experienced by Users immediately from the Homepage, by clicking one of the four selection choices. Each chapter has its own personality, narrative and unique twist. 
+        
+        </p>
+
   </Container>
+        <Image className="heroImage2" src={space22heroTwo}/>
       
+        <Container text>
+    <Header className="linkEndTitle" as='h2' >View This Project</Header>
+        <div className="blockLink">
+        <Link to={'/https://victordoyle.github.io/NarrativePage1'}>
+        <Icon name="external alternate" size="big" className="contentLinks"/>
+        </Link>
+
        
+        <Link to={'/https://github.com/VictorDoyle/NarrativePage1'}>
+        <Icon name="github" size="big" className="contentLinks"/>
+        </Link>
+        
+        <Link to={'/https://open.spotify.com/artist/1PBlbMf53P99M1R7eNFxIR?si=24m18Gz2RgeR5CU7ZbE-Gg'}>
+        <Icon name="spotify" size="big" className="contentLinks"/>
+        </Link>
+        </div>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+       
+
+
+  </Container>
 
        
       
